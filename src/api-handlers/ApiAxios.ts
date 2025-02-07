981106c236d622f82a08b8569d797d00249786ae
khuzaimazammarud <khuzaimazammarud@gmail.com>
2025-02-07 15:36:47 +0500

    solved cors issue

--- src/api-handlers/ApiAxios.ts
+++ src/api-handlers/ApiAxios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const APIAxios = axios.create({
   baseURL: 'https://whiplano-1b8102db6480.herokuapp.com', 
   headers: {
-      'Content-Type': 'application/json', // Set the default content type for requests
+      "Content-Type": "application/x-www-form-urlencoded", // Set the default content type for requests
     // 'ngrok-skip-browser-warning': '1',
   },
 });
